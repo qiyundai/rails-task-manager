840c7ff62320966fb26c317ca5863e452c14010e
Qiyun Dai <pianoquin@gmail.com>
2021-02-12 22:23:37 -0600

    create done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/new", to: "tasks#create", as: :new_task
+  post "tasks", to: "tasks#create"
 end
